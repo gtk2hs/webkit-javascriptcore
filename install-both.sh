2f55334d9fff4d1d125bf815b69a37dbc9b4a8d9
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2014-07-28 06:07:18 +1200

    Fix travis build script

--- install-both.sh
+++ install-both.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -ex
 
-$CABAL clean
+cabal clean
 mv webkit-javascriptcore.cabal-renamed webkit-javascriptcore.cabal || true
 mv webkitgtk3-javascriptcore.cabal webkitgtk3-javascriptcore.cabal-renamed || true
-$CABAL install "$@"
+cabal install "$@"
 
-$CABAL clean
+cabal clean
 mv webkitgtk3-javascriptcore.cabal-renamed webkitgtk3-javascriptcore.cabal || true
 mv webkit-javascriptcore.cabal webkit-javascriptcore.cabal-renamed || true
-$CABAL install "$@"
+cabal install "$@"
 
