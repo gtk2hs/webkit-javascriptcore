f5bd76010d2650b851f05625fb57d2daa278b42e
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2020-07-12 15:18:46 +1200

    Update nix and fix Cabal upper bound

--- default.nix
+++ default.nix
@@ -1,16 +1,10 @@
-{ pkgs ? import nixpkgs (haskellNixpkgsArgs // {
-    overlays = haskellNixpkgsArgs.overlays ++ [ (import ./nix/overlays/gtk-debug.nix) ];
-  } // (if system == null then {} else { inherit system; }))
-, nixpkgs ? haskellNixSrc + "/nixpkgs"
-, haskellNixpkgsArgs ? import haskellNixSrc
-, haskellNixSrc ? builtins.fetchTarball {
-    url = "https://github.com/input-output-hk/haskell.nix/archive/1dd6951d6a56115fd368c5b37b8dab47a90ec45c.tar.gz";
-    sha256 = "02nhjy90xwg9wrp81cnlczndxags966c71wlbz6n91423a13mc99";
-  }
-, haskellCompiler ? "ghc881"
+{ sourcesOverride ? {}
+, sources ? import ./nix/sources.nix {} // sourcesOverride
+, pkgs ? (import sources."haskell.nix" {}).pkgs
+, compiler-nix-name ? "ghc8101"
 , system ? null
 }:
-  pkgs.haskell-nix.cabalProject' {
-    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-    ghc = pkgs.haskell-nix.compiler.${haskellCompiler};
+  pkgs.haskell-nix.project {
+    inherit compiler-nix-name;
+    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; name = "jsaddle"; };
   }
